a3dccba317ef5846b0df0edebe7ef6a2c595815c
Maite Abril Agrech <maiabrill2004@gmail.com>
2026-08-17 17:49:49 -0300

    New menu with sorting, including js

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+const filterButtons = document.querySelectorAll(".menu-filter");
+const menuItems = document.querySelectorAll(".menu-item");
+
+filterButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const selectedCategory = button.dataset.category;
+
+    // Update active button
+
+    filterButtons.forEach((filterButton) => {
+      filterButton.classList.remove("menu-filter--active");
+    });
+
+    button.classList.add("menu-filter--active");
+
+    // Filter menu items
+
+    menuItems.forEach((item) => {
+      const itemCategory = item.dataset.category;
+
+      if (selectedCategory === "all" || selectedCategory === itemCategory) {
+        item.style.display = "grid";
+      } else {
+        item.classList.add("menu-item--hidden");
+      }
+    });
+  });
+});
