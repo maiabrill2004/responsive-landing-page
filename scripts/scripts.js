b356d6636ad17b8e2529aaace66523602fb72058
Maite Abril Agrech <maiabrill2004@gmail.com>
2026-08-18 02:45:09 -0300

    Improving arquitechture creating dinamic menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,43 @@
+//Creating menu
+import { menuItemsList } from "./data/menu-data.js";
+
+const menuList = document.querySelector("#menu-list");
+
+function renderMenu(items) {
+  menuList.innerHTML = "";
+
+  items.forEach((item) => {
+    const article = document.createElement("article");
+
+    article.classList.add("menu-item");
+    article.dataset.category = item.category;
+
+    article.innerHTML = `
+      <div class="menu-item-content">
+
+        <div>
+          <span class="menu-item-category">
+            ${item.category.toUpperCase()}
+          </span>
+
+          <h3>${item.name}</h3>
+        </div>
+
+        <p>${item.description}</p>
+
+      </div>
+
+      <span class="menu-item-price">
+        ${item.price}
+      </span>
+    `;
+
+    menuList.appendChild(article);
+  });
+}
+
+renderMenu(menuItemsList);
+//Sorting Feature Menu
 const filterButtons = document.querySelectorAll(".menu-filter");
 const menuItems = document.querySelectorAll(".menu-item");
 
@@ -21,7 +61,7 @@ filterButtons.forEach((button) => {
       if (selectedCategory === "all" || selectedCategory === itemCategory) {
         item.style.display = "grid";
       } else {
-         item.style.display = "none";
+        item.style.display = "none";
       }
     });
   });
